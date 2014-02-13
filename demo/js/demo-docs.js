8809d0571235acee2419248077e4cb687cf3c6a3
seven-phases-max <seven.phases.max@gmail.com>
2014-02-13 22:02:28 +0400

    Updated hover demo.

--- demo/js/demo-docs.js
+++ demo/js/demo-docs.js
@@ -10,16 +10,31 @@ function main() {
     if (!rules)
         return;
 
-    var i, n = rules.length, re = /\.([\w-]+)(:hover)/, selector,
-        body   = $('body'),
-        button = $('<button type="button" class="btn btn-primary btn-lg"></button>');
-
+    var i, j = -1, n = rules.length, re = /\.([\w-]+)(:hover)/, selector, prevSelector,
+        container = $('#content'),
+        header    = $('<h3 class="sub-header"></h3>'),
+        button    = $('<button type="button" class="btn btn-primary btn-lg"></button>'),
+        groupClass = [
+            "btn-success",
+            "btn-primary",
+            "btn-warning",
+            "btn-danger",
+            "btn-info"
+        ];
+        
     for (i = 0; i < n; i++) {
         if (rules[i].type !== 1 
-            || !(selector = re.exec(rules[i].selectorText)))
+            || !(selector = re.exec(rules[i].selectorText))
+            || (prevSelector === (selector = selector[1])))
                 continue;
-        selector = selector[1];
-        console.log(selector);
-        button.clone().appendTo(body).addClass(selector).text(selector);
+        prevSelector = selector;
+        if (!selector.search("md-group")) {
+            header.clone().appendTo(container)
+                .text(rules[i].style.getPropertyValue('content').replace(/'/g, ""));
+            j = (j + 1) % groupClass.length;
+        } else {
+            button.clone().appendTo(container)
+                .addClass(groupClass[j]).addClass(selector).text(selector);
+        }
     }
 }
