d538a7aff0af1cb4bae49644025458da8e6a7504
seven-phases-max <seven.phases.max@gmail.com>
2014-02-16 19:32:15 +0400

    Added theme switcher.

--- js/demo-docs.js
+++ js/demo-docs.js
@@ -1,25 +1,58 @@
-$(document).ready(main);
 
-function main() {
-    //var styles = $('style');
-    //console.log(styles.contents());
-    var rules = document.styleSheets
-             && document.styleSheets[2]
-             && document.styleSheets[2].cssRules;
+var themes = [
+    ["Default",  "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"],
+    ["Amelia",   "//bootswatch.com/amelia/bootstrap.min.css"],
+    // ["Cerulean", "//bootswatch.com/cerulean/bootstrap.min.css"],
+    ["Cosmo",    "//bootswatch.com/cosmo/bootstrap.min.css"],
+    ["Cyborg",   "//bootswatch.com/cyborg/bootstrap.min.css"],
+    ["Flatly",   "//bootswatch.com/flatly/bootstrap.min.css"],
+    ["Journal",  "//bootswatch.com/journal/bootstrap.min.css"],
+    ["Lumen",    "//bootswatch.com/lumen/bootstrap.min.css"],
+    ["Readable", "//bootswatch.com/readable/bootstrap.min.css"],
+    // ["Simplex",  "//bootswatch.com/simplex/bootstrap.min.css"],
+    // ["Slate",    "//bootswatch.com/slate/bootstrap.min.css"],
+    // ["Spacelab", "//bootswatch.com/spacelab/bootstrap.min.css"],
+    ["United",   "//bootswatch.com/united/bootstrap.min.css"],
+    ["Yeti",     "//bootswatch.com/yeti/bootstrap.min.css"]
+];
+
+$(function(){
+    var sheet = $('<link href="' + themes[0][1] + '" rel="stylesheet" />'),
+        list  = $('#theme-list');
+    sheet.prependTo('head');
     
-    if (!rules)
+    if (!list.length)
+        return;
+    
+    var i, n = themes.length, name, href;
+    for (i = 0; i < n; i++) {
+        name = themes[i][0];
+        href = themes[i][1];
+        $('<li><a href="#" data-href="' + href + '">' + name + '</a></li>')
+            .appendTo(list).find('a').click(function() {
+                sheet.attr('href', $(this).attr('data-href'));
+            });
+    }
+});
+
+function generateButtons(file) {
+    var i = 0, sheet, sheets = document.styleSheets;
+    while ((sheet = sheets[i++]) && (file !== sheet.href.split('/').pop())); // !
+    if (!sheet)
         return;
 
-    var i, j = -1, n = rules.length, re = /\.([\w-]+)(:hover)/, selector, prevSelector,
-        container = $('#content'),
+    var rules = sheet.cssRules, n = rules.length, j = -1, 
+        re = /\.([\w-]+)(:hover)/, selector, prevSelector,
+        container = $('#showcase'),
         header    = $('<h3 class="sub-header"></h3>'),
-        button    = $('<button type="button" class="btn btn-primary btn-lg"></button>'),
+        button    = $('<button type="button" class="btn btn-lg"></button>'),
         groupClass = [
-            "btn-success",
-            "btn-warning",
-            "btn-danger",
             "btn-info",
-            "btn-primary"
+            "btn-success",
+            "btn-danger",
+            "btn-warning",
+            "btn-primary",
+            "btn-default"
         ];
         
     for (i = 0; i < n; i++) {
